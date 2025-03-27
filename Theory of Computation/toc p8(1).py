6f0da444e4372d9be6ce56bade5bdaebc7b36439
Krane <93140271+codeByKaustubh@users.noreply.github.com>
2025-03-28 04:09:09 +0530

    Update toc p8(1).py

--- Theory of Computation/toc p8(1).py
+++ Theory of Computation/toc p8(1).py
@@ -1,11 +1,11 @@
 #Program 1:  Design a python program for creating a machine which count number of 1’s and 0’s in a given string.  
-def count_ones_zeroes(binary_string):
-    ones_count = binary_string.count('1')
-    zeroes_count = binary_string.count('0')
-    return ones_count, zeroes_count
+def count(binary_string):
+    onec = binary_string.count('1')
+    zeroc = binary_string.count('0')
+    return onect, zeroc
 
 if __name__ == "__main__":
     binary_string = input("Enter a binary string: ")
-    ones, zeroes = count_ones_zeroes(binary_string)
-    print(f"Number of 1's: {ones}")
-    print(f"Number of 0's: {zeroes}")
+    one, zero = count(binary_string)
+    print(f"Number of 1's: {one}")
+    print(f"Number of 0's: {zero}")
