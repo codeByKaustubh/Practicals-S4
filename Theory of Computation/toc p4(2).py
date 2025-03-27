56cba87cc361fde4550419cf21bc04f3c636ee0a
Krane <93140271+codeByKaustubh@users.noreply.github.com>
2025-03-27 20:25:22 +0530

    Update toc p4(2).py

--- Theory of Computation/toc p4(2).py
+++ Theory of Computation/toc p4(2).py
@@ -1,26 +1,26 @@
 #Program 2: Design a Program for creating machine that accepts three consecutive a’s.
-class ThreeConsecutiveZerosFSM:
+class ThreeConsecutiveAsFSM:
     def __init__(self):
         self.state = 0  
     
     def transition(self, input_char):
         if self.state == 0:
-            if input_char == '0':
+            if input_char == 'a':
                 self.state = 1
             else:
                 self.state = 0
         elif self.state == 1:
-            if input_char == '0':
+            if input_char == 'a':
                 self.state = 2
             else:
                 self.state = 0
         elif self.state == 2:
-            if input_char == '0':
+            if input_char == 'a':
                 self.state = 3  
             else:
                 self.state = 0
         elif self.state == 3:
-            if input_char == '0':
+            if input_char == 'a':
                 self.state = 3
             else:
                 self.state = 0
@@ -33,7 +33,7 @@ class ThreeConsecutiveZerosFSM:
                 return True  
         return False
 
-fsm = ThreeConsecutiveZerosFSM()
-test_cases = ["1100011", "1111", "000", "10001", "1100001"]
+fsm = ThreeConsecutiveAsFSM()
+test_cases = ["bbaaabb", "bbbb", "aaa", "aabaa", "baaaab"]
 for test in test_cases:
     print(f"Input: {test}, Accepted: {fsm.accepts(test)}")
